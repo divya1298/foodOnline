5978c05eae2abdb12f61e96203c96fa458621d93
Divya <divyaayvid21@gmail.com>
2023-09-16 20:51:15 +0530

    Restaurant profile set-up

--- foodOnline_main/vendor/forms.py
+++ foodOnline_main/vendor/forms.py
@@ -3,6 +3,8 @@ from .models import Vendor
 
 
 class VendorForm(forms.ModelForm):
+    vendor_license = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn.btn-info'}))
+
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
